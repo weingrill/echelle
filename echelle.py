ab6e8a21db4b229f11fad3cbf6216ebc8d41b4a6
Joerg Weingrill <jweingrill@gmailcom>
2018-09-13 22:09:04 +0200

    echelle: outlier removal, dark subtraction, background subtraction added stackoverflow

--- echelle.py
+++ echelle.py
@@ -2,21 +2,40 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
+from stackoverflow import find_outlier_pixels
 
 class Echelle(object):
     def __init__(self):
         self.data = None
+        self.dark = None
 
     def loadimage(self, filename):
         hdu = fits.open(filename)
         self.data = hdu[0].data
-        # normalization
-        self.data = 1.0*self.data - np.min(self.data)
-        self.data /= np.max(self.data)
 
     def saveimage(self, filename):
         hdu = fits.PrimaryHDU(self.data)
         hdu.writeto(filename, overwrite=True)
+
+    def subtract_dark(self, filename):
+        hdu = fits.open(filename)
+        self.dark = hdu[0].data
+        hot_pixels, self.dark = find_outlier_pixels(self.dark, worry_about_edges=True)
+        print(hot_pixels)
+        self.data = self.data - self.dark
+        i = np.where(self.data < 0)
+        self.data[i] = 0.0
+        i = np.where(self.data > 55000)
+        self.data[i] = 0.0
+
+    def normalize(self):
+        # normalization
+        self.data = 1.0 * self.data - np.min(self.data)
+        self.data /= np.max(self.data)
+
+    def remove_hot_pixels(self):
+        hot_pixels, self.data = find_outlier_pixels(self.data, worry_about_edges=True)
+        print(hot_pixels)
 
     def artificial_flat(self):
         numrows, numcols = self.data.shape
@@ -45,22 +64,19 @@ class Echelle(object):
         X, Y = np.meshgrid(x, y, copy=False)
         flat = coeff[0] + coeff[1]*X + coeff[2]*Y + coeff[3]*X**2 + coeff[4]*Y**2 + coeff[5]*X*Y
         # normalize the flat since we get negative values
-        # flat -= np.min(flat)
-        # flat /= np.max(flat)
-        # plt.imshow(flat)
-        # plt.show()
-        # plt.close()
+        flat -= np.min(flat)
+        flat /= np.max(flat)
+        flat += 0.01
         hdu = fits.PrimaryHDU(flat)
         hdu.writeto('/Users/jwe/Projects/Astronomie/Spectra/artificial_flat.fit', overwrite=True)
-        # self.data -= np.min(self.data)
-        # self.data /= np.max(self.data)
 
         self.data /= flat
-        #
-        # self.data /= np.nanmax(self.data)
-        plt.imshow(self.data, vmin=-1.0, vmax=1.0)
-        plt.show()
-        plt.close()
+        self.data -= np.nanmin(self.data)
+        self.data /= np.nanmax(self.data)
+
+        # plt.imshow(self.data, vmin=0.0, vmax=1.0)
+        # plt.show()
+        # plt.close()
 
     def extract_orders(self):
         numrows, numcols = self.data.shape
@@ -82,8 +98,7 @@ class Echelle(object):
             # plt.plot(np.arange(-window, window), cv[mid - window + p: mid + window + p])
         # plt.show()
         # plt.grid()
-        z = np.polyfit(cols, c, 2)
-        p = np.polyval(z, cols)
+        c
         # plt.plot(c)
         # plt.plot(p)
         # plt.show()
@@ -97,17 +112,39 @@ class Echelle(object):
             # print(offset, offset+numrows, data[:, i].shape)
             new_data[offset: offset + numrows, i] = self.data[:, i]
         # plt.imshow(np.log(new_data+1.0))
-        plt.plot(new_data[:, 800])
-        plt.show()
-        plt.close()
-        plt.plot(np.mean(new_data[620: 640, :], 0))
-        plt.plot(np.mean(new_data[681: 703, :], 0))
-        plt.show()
-        plt.close()
+        # plt.plot(np.mean(new_data[620: 640, :], 0))
+        # plt.plot(np.mean(new_data[681: 703, :], 0))
+        # plt.show()
+        # plt.close()
+        self.data = new_data
+
+    def subtract_background(self):
+        numrows, numcols = self.data.shape
+        background = np.zeros_like(self.data)
+        for col in np.arange(numcols):
+            row = self.data[:, col]
+            i = np.where((row < np.mean(row)) & (row > 0.0))
+            y = np.arange(numrows)
+            z = np.polyfit(y[i], row[i], 5)
+            p = np.polyval(z, y)
+            background[:, col] = p
+            # plt.plot(row)
+            # plt.plot(y[i], row[i],',')
+        hdu = fits.PrimaryHDU(flat)
+        hdu.writeto('/Users/jwe/Projects/Astronomie/Spectra/artificial_background.fit', overwrite=True)
+
+        # plt.plot(y, p)
+        # plt.show()
+        # plt.close()
+
 
 
 e = Echelle()
 e.loadimage('/Users/jwe/Projects/Astronomie/Spectra/spectrum7.fit')
+e.subtract_dark('/Users/jwe/Projects/Astronomie/Spectra/dark10s.fit')
+e.saveimage('/Users/jwe/Projects/Astronomie/Spectra/spectrum7_dark.fit')
+
+e.normalize()
 e.artificial_flat()
 e.saveimage('/Users/jwe/Projects/Astronomie/Spectra/spectrum7_flat.fit')
-# e.extract_orders()
+e.extract_orders()
